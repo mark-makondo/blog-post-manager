4deb4b6213395987d239430f591895631695a51e
Mark Makondo <markalbert.makondo@gmail.com>
2021-02-20 20:59:50 +0800

    post method added

--- src/components/post-method/PostMethod.js
+++ src/components/post-method/PostMethod.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const PostMethod = ({ isGuest }) => {
+const PostMethod = ({ isGuest, isPostHolderActive, editableCLickHandler }) => {
+
     return (
         <div className="post-method">
             <select className="normal-1" name="order" id="order">
                 <option value="asc">ASC</option>
                 <option value="desc">DESC</option>
             </select>
-            <button  className="normal-1" disabled={isGuest}>edit</button>
-            <button  className="normal-1" disabled={isGuest}>delete</button>
+            <button className="normal-1" disabled={ isGuest || isPostHolderActive ? true : false } onClick={e =>editableCLickHandler(e)}>edit</button>
+            <button className="normal-1" disabled={ isGuest || isPostHolderActive ? true : false }>delete</button>
         </div>
     )
 }
--- src/components/post-method/PostMethodContainer.js
+++ src/components/post-method/PostMethodContainer.js
@@ -8,12 +8,18 @@ import PostMethodUI from './PostMethod.js';
 
 const PostMethodContainer = ({ isPostHolderActive, setIsEditable }) => {
 
+    const editableCLickHandler = (e) =>{
+        e.preventDefault();
+        setIsEditable(true);
+    }
     
     return (
         <UserContext.Consumer>
             { isGuest => (
                 <PostMethodUI
                     isGuest = {isGuest}
+                    isPostHolderActive =  {isPostHolderActive}
+                    editableCLickHandler = {editableCLickHandler}
                 />
             )}
        </UserContext.Consumer>
