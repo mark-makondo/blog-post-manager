90473def0e79531b42642fcefe7946acb96abf38
Mark Makondo <markalbert.makondo@gmail.com>
2021-02-23 11:20:36 +0800

    configured browser router for ghpages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import AdminLoginContainer from './components/admin-login/AdminLoginContainer.js
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Switch>
           <Route exact path='/' component={LoginPageContainer}/>
